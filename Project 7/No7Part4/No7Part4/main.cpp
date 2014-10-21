87af30a9944171d501864a482ffa6536243efd10
Sergio Gomez <sep199614@hotmail.com>
2014-10-21 16:40:24 -0700

    Update main.cpp

--- Project 7/No7Part4/No7Part4/main.cpp
+++ Project 7/No7Part4/No7Part4/main.cpp
@@ -1,38 +1,52 @@
-//===========================================
-//  File: No7Part4.cpp
-//===========================================
-//  Programmer: Sergio Gomez
-//  Date: 10/20/2014
-//  Class: CPSC 121
-//  Time: 2:30 TR
-//  Instructor: Dr. Ray Ahmadnia
-//  Project: No.7 Part 4
+//==============================================
+//	File: No7Part4.cpp
+//==============================================
+//	Programmer: Sergio Gomez
+//	Date: 10/21/2014
+//	Class: CPSC 121
+//	Time: 2:30 TR
+//	Instructor: Dr. Ray Ahmadnia
+//	Project: No. 7 Part 4
 //
-//  Description:
-//
-//===========================================
+//	Description:
+//		Utilizes exponential function to learn 
+//		recursive function
+//==============================================
 
+// declare variable
 #include <iostream>
 #include <cmath>
+#include <ctime>
 
 using namespace std;
 
-int exponential(int base, int n);
+// function prototype
+int exponential(int n);
 
 int main()
 {
-    cout << "start";
-    cout << exponential(3, 5);
-    cout <<endl;
-    cout << "stop\n";
-    
-    return 0;
+	// displays today's date and time
+	time_t p; time(&p);
+	cout << "Today's date and time is: ";
+	cout << ctime(&p) << endl;
+
+	// determines the 10th term of base 3
+	cout << "3^10 = ";
+	cout << exponential(10);
+	cout << endl;
+
+	//terminate program
+	return 0;
 }
 
-int exponential(int base, int n)
+int exponential(int n)
 {
-    if (n == 1)
-        return pow(float(base), n);
-    else
-        return exponential(base, n++);
+	if(n == 2) return 9;
+	else return pow(3., n) + exponential(n-1);
 }
+/*==================Output===================
+Today's date and time is: Tue Oct 21 16:39:46 2014
+
+3^10 = 88569
+Press any key to continue . . .
+============================================*/
