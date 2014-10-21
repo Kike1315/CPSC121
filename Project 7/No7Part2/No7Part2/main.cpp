247864f3d584f59c3925bb07ff0ca19cd1b6b54a
Sergio Gomez <segomez1304@hotmail.com>
2014-10-21 11:07:22 -0700

    Project
    Homework

--- Project 7/No7Part2/No7Part2/main.cpp
+++ Project 7/No7Part2/No7Part2/main.cpp
@@ -1,32 +1,57 @@
 //============================================
 //  File: No7Part2.cpp
 //============================================
+//  Programmer: Sergio Gomez
+//  Date: 10/20/2014
+//  Class: CPSC 121
+//  Trime: 2:30 TR
+//  Instructor: Dr. Ray Ahmadnia
+//  Project: No.7 Part 2
+//
+//  Description:
+//      This program utilizes txt file to
+//      practice the use of dynamic arrays
 //============================================
 
+// declare libraries
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 
 using namespace std;
 
+// creates structure person
 struct PERSON {
     int age; float gpa; char name[20];
 };
 
+// function prototypes
 void numFiles(string fname, int& n);
 void readData(string fname, PERSON x[], int n);
 void display(PERSON x[], int n);
 
+// main program
 int main()
 {
+    // displays today's date and time
+    time_t k; time(&k);
+    cout << "Today's date and time is: ";
+    cout << ctime(&k) << endl;
+    
+    // delares variable len
     int len;
+    // determines length of file
     numFiles("two.txt", len);
 
+    // creates person with length of file
     PERSON* p = new PERSON[len];
+    // puts data from file to person
     readData("two.txt", p, len);
     
+    // displays data in a table
     display(p, len);
     
+    // terminates program
     return 0;
 }
 
@@ -75,3 +100,16 @@ void display(PERSON x[], int n)
         cout << x[i].name << endl;
     }
 }
+
+/*=======================Output====================
+ Today's date and time is: Tue Oct 21 11:02:18 2014
+ 
+ Age  GPA                 Name
+ ______________________________
+ 22   2.2          Bill Clinton
+ 33   3.3          Phil Jackson
+ 44   4.0        Mary A Johnson
+ 25   2.5            Adam Smith
+ 65   3.2         George W Bush
+ Program ended with exit code: 0
+ ================================================*/
--- Project 7/No7Part3/No7Part3/main.cpp
+++ Project 7/No7Part3/No7Part3/main.cpp
@@ -19,27 +19,42 @@ using namespace std;
 
 int main()
 {
+    // displays today's date and time
+    time_t p; time(&p);
+    cout << "Today's date and time is: ";
+    cout << ctime(&p) << endl;
+    
+    // declares a variable name
     char name[15];
+    // prompts user for full name
     cout << "Enter your full name: ";
     cin.get(name, 15, '\n');
     
+    // determines length of user's name
     cout << "\tYour name's length is ";
     cout << strlen(name);
     cout << endl;
     
+    // displays user's name in lower case
     cout << "\tYour name in lower case: ";
-    strlow(name);
+    strlwr(name);
     cout << name;
     cout << endl;
     
+    // displays user's name in upper case
     cout << "\tYour name in upper case: ";
     strupr(name);
     cout << name;
     cout << endl;
     
+    // displays user's first name
     cout << "\tYour first name: ";
     
     cout << endl;
     
     return 0;
 }
+
+/*===============Output=================
+
+ =====================================*/
